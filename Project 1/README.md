916b131f67696ef266c88b06d1d222d091a24c94
Gromovoi Stanislav <gromila12@rambler.ru>
2023-07-24 10:10:56 +0600

    Project 1

---
+++ Project 1/README.md
@@ -0,0 +1,25 @@
+# NodeJS gRPC and ExpressJS
+
+Just a quick example of a gRPC server made with NodeJS and client ExpressJS<br>
+It contains 2 different examples:
+
+1. Unary
+2. Server Streaming
+
+### Runs the gRPC server
+
+```
+nmp run server
+```
+
+### Runs client
+
+```
+nmp run client
+```
+
+### Runs client express
+
+```
+nmp run client-express
+```
--- Project 1/client-express.js
+++ Project 1/client-express.js
@@ -7,65 +7,66 @@ const grpcObject = grpc.loadPackageDefinition(packageDef);
 const todoPackage = grpcObject.todoPackage;
 
 const grpcServerAddress = 'localhost:40000';
+const grpcClient = new todoPackage.Todo(
+  grpcServerAddress,
+  grpc.credentials.createInsecure()
+);
 
 app.use(express.json());
 
 // Endpoint to create a new todo item
 app.post('/createTodo', (req, res) => {
-	const text = req.body.text;
-	const client = new todoPackage.Todo(grpcServerAddress, grpc.credentials.createInsecure());
-	client.createTodo(
-		{
-			text: text,
-		},
-		(err, response) => {
-			if (err) {
-				console.error('Error creating todo:', err);
-				res.status(500).json({ error: 'Error creating todo' });
-			} else {
-				console.log('Created new item: ' + JSON.stringify(response));
-				res.json(response);
-			}
-		}
-	);
+  const text = req.body.text;
+  grpcClient.createTodo(
+    {
+      text: text,
+    },
+    (err, response) => {
+      if (err) {
+        console.error('Error creating todo:', err);
+        res.status(500).json({ error: 'Error creating todo' });
+      } else {
+        console.log('Created new item: ' + JSON.stringify(response));
+        res.json(response);
+      }
+    }
+  );
 });
 
 // Endpoint to read all todos
 app.get('/readTodos', (req, res) => {
-	const client = new todoPackage.Todo(grpcServerAddress, grpc.credentials.createInsecure());
-	client.readTodos({}, (err, response) => {
-		if (err) {
-			console.error('Error reading todos:', err);
-			res.status(500).json({ error: 'Error reading todos' });
-		} else {
-			console.log('Received all todos from server:', response);
-			res.json(response);
-		}
-	});
+  grpcClient.readTodos({}, (err, response) => {
+    if (err) {
+      console.error('Error reading todos:', err);
+      res.status(500).json({ error: 'Error reading todos' });
+    } else {
+      console.log('Received all todos from server:', response);
+      res.json(response);
+    }
+  });
 });
 
 // Endpoint to read todos with streaming response
 app.get('/readTodosStream', (req, res) => {
-	const client = new todoPackage.Todo(grpcServerAddress, grpc.credentials.createInsecure());
-	const call = client.readTodosStream({});
-	const todos = [];
+  const call = grpcClient.readTodosStream({});
+  const todos = [];
 
-	call.on('data', (todo) => {
-		todos.push(todo);
-	});
+  call.on('data', (todo) => {
+    todos.push(todo);
+  });
 
-	call.on('end', () => {
-		console.log('Todos stream read successfully:', todos);
-		res.json({ items: todos });
-	});
+  call.on('end', () => {
+    console.log('Todos stream read successfully:', todos);
+    res.json({ items: todos });
+  });
 
-	call.on('error', (err) => {
-		console.error('Error reading todos stream:', err);
-		res.status(500).json({ error: 'Error reading todos stream' });
-	});
+  call.on('error', (err) => {
+    console.error('Error reading todos stream:', err);
+    res.status(500).json({ error: 'Error reading todos stream' });
+  });
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-	console.log(`Express server listening on http://localhost:${PORT}`);
+  console.log(`Express server listening on http://localhost:${PORT}`);
 });
