ee239048b98f5db4788445b21d6c60d3e61d910f
Gromovoi Stanislav <gromila12@rambler.ru>
2023-08-02 17:40:56 +0600

    Project 5

---
+++ Project 5/README.md
@@ -0,0 +1,13 @@
+# NodeJS + gRPC
+
+## Running the App
+
+```
+npm run start:server
+```
+
+```
+npm run start:client Name
+or
+npm run start:client  -- --target=localhost:50051 Name
+```
---
+++ Project 5/client.js
@@ -0,0 +1,48 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const parseArgs = require('minimist');
+
+const PROTO_PATH = __dirname + '/welcome.proto';
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+
+const welcome_proto = grpc.loadPackageDefinition(packageDefinition).welcome;
+
+function main() {
+  const argv = parseArgs(process.argv.slice(2), {
+    string: 'target',
+  });
+
+  let target;
+
+  if (argv.target) {
+    target = argv.target;
+  } else {
+    target = 'localhost:50052';
+  }
+
+  const client = new welcome_proto.Welcome(
+    target,
+    grpc.credentials.createInsecure()
+  );
+
+  let user;
+
+  if (argv._.length > 0) {
+    user = argv._[0];
+  } else {
+    user = 'Tom Tomson';
+  }
+
+  client.SayWelcome({ name: user }, function (err, response) {
+    console.log('Message:', response.message);
+  });
+}
+
+main();
